f806f14bfb9895510e486bcb87b8110e8356d222
Krithik Acharya <krithikacharya@gatech.edu>
2020-08-08 20:01:19 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/whatstheweather/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.whatstheweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.concurrent.ExecutionException;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView cityText;
+    TextView weatherTextView;
+
+    public class JSONDownload extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                String result = "";
+
+                URL url = new URL(urls[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+                }
+
+                return result;
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Error";
+            }
+        }
+        @Override
+        protected void onPostExecute (String s){
+            super.onPostExecute(s);
+            try {
+                String main = "";
+                String description = "";
+                JSONObject jsonObject = new JSONObject(s);
+                String weatherInfo = jsonObject.getString("weather");
+                JSONArray arr = new JSONArray(weatherInfo);
+                for (int i = 0; i < arr.length(); i++) {
+                    JSONObject jsonPart= arr.getJSONObject(i);
+                    main = jsonPart.getString("main");
+                    description = jsonPart.getString("description");
+                }
+                weatherTextView.setText(main + ": " + description);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "No weather data available", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void weatherButton(View view) throws UnsupportedEncodingException {
+
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+        JSONDownload task = new JSONDownload();
+        String result = null;
+        String temp = cityText.getText().toString();
+        weatherTextView.setText("");
+        String restUrl = URLEncoder.encode(temp, "UTF-8");
+
+        try {
+            if (!(restUrl.equals(""))) {
+                result = task.execute("https://openweathermap.org/data/2.5/weather?q=" + restUrl + "&appid=439d4b804bc8187953eb36d2a8c26a02").get();
+            } else {
+                Toast.makeText(this, "Please enter a city", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+
+        cityText = (TextView) findViewById(R.id.cityPlainText);
+        weatherTextView = (TextView) findViewById(R.id.weatherTextView);
+
+        cityText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                weatherTextView.setText("");
+            }
+        });
+
+    }
+}
